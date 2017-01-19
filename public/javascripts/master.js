0eb38279eea78f96a94d9209621b33ba701136cf
Tim Stone <tmslft@gmail.com>
2017-01-19 09:58:27 -0500

    Remove pagesize parameter for /badges call
    (see previous commit)

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -473,7 +473,7 @@ var StackElections = (function () {
         */
         stalkMetaCandidates(users, true);
 
-        Meddle.With(Settings.siteName).users(users).badges().pagesize(100).all().sort('type').max('named').filter('3gY1MumGZ6PD').get(function (badges) {
+        Meddle.With(Settings.siteName).users(users).badges().all().sort('type').max('named').filter('3gY1MumGZ6PD').get(function (badges) {
             var lists = $('.badge_list ul:empty');
 
             StackElections.options.selectedBadges.forEach(function (badge) {
