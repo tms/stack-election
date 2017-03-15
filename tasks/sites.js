6187a9ce10408054371531ec6bbfd52ffb6f0433
Tim Stone <tmslft@gmail.com>
2017-03-15 11:35:10 -0400

    Update site metadata when it changes

--- tasks/sites.js
+++ tasks/sites.js
@@ -7,15 +7,23 @@ exports.update = function update(callback) {
             changed = false;
 
         sites && sites.forEach(function (site) {
-            if (site.site_type === 'main_site' && !stored[site.site_url]) {
-                stored[site.site_url] = {
-                    favicon: site.favicon_url,
-                    icon: site.icon_url,
-                    name: site.name,
-                    elections: []
-                };
+            if (site.site_type === 'main_site') {
+                var existing = stored[site.site_url],
+                    elections = existing ? existing.elections : [];
 
-                changed = true;
+                if (!existing
+                        || existing.favicon != site.favicon_url
+                        || existing.icon != site.icon_url
+                        || existing.name != site.name) {
+                    stored[site.site_url] = {
+                        favicon: site.favicon_url,
+                        icon: site.icon_url,
+                        name: site.name,
+                        elections: elections
+                    };
+
+                    changed = true;
+                }
             }
         });
 
